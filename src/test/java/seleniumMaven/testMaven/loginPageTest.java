926b4a3dc718f8002c0f3f0616a7478ae82c4f7f
hmdraza9 <hmdraza9@gmail.com>
2020-07-03 21:50:00 +0530

    changed test file to specific to page

--- src/test/java/seleniumMaven/testMaven/loginPageTest.java
+++ src/test/java/seleniumMaven/testMaven/loginPageTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
-public class AppTest extends App{
+public class loginPageTest extends App{
 
 	@AfterTest
 	public void afterTesting() {
